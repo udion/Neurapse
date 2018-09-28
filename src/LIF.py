a548181fa37ca9a56c04edf24c56f29a06bbcd02
udion <uddeshya.upa@gmail.com>
2018-09-28 16:34:29 +0530

    LIF with refactory, CURRENTS

--- src/LIF.py
+++ src/LIF.py
@@ -2,13 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class LIF():
-    def __init__(self, C, gL, V_thresh, El, num_neurons=1):
+    def __init__(self, C, gL, V_thresh, El, Rp, num_neurons=1):
         self.C = C
         self.gL = gL
         self.V_thresh = V_thresh
         self.El = El
+        self.Rp = Rp
         self.num_neurons = num_neurons
         self.fireflag = [False]*num_neurons
+        self.refract = [None]*num_neurons
     
     def compute(self, V0, I, delta_t):
         '''
@@ -16,12 +18,15 @@ class LIF():
             V0 : initial memberane potential [num_neurons, ]
         '''
         self.delta_t = delta_t
-        n_t = I.shape[1]
+        self.n_t = I.shape[1]
+        self.n_Rp = int(self.Rp/self.delta_t)
+        print('n_rp ', self.n_Rp)
+        # return
         V = []
         Vi = V0
         print(Vi.shape)
         V.append(Vi)
-        for i in range(n_t):
+        for i in range(self.n_t):
             Vi = self.update_fn(Vi, I[:,i].reshape(self.num_neurons, 1))
             V.append(Vi)
         V = np.concatenate(V, axis=1)
@@ -34,6 +39,16 @@ class LIF():
             if f == True: #overide the calculation with the reset potential
                 self.fireflag[idx] = False
                 V_i1[idx] = self.El
+                self.refract[idx] = self.n_Rp
+        # print(self.refract)
+        
+        for idx, r in enumerate(self.refract):
+            if r != None:
+                V_i1[idx] = self.El
+                if self.refract[idx]-1 <= 0:
+                    self.refract[idx] = None
+                else:
+                    self.refract[idx] = self.refract[idx]-1
         
         for idx, v in enumerate(V_i1):
             if v[0] >= self.V_thresh: #fire
@@ -49,6 +64,8 @@ V_thresh=20*(10**-3)
 El=-70*(10**-3)
 delta_t = 0.1*(10**-3)
 T = 500*(10**-3)
+Rp = 2*(10**-3)
+# Rp = 0 
 
 # Ic0 = np.array([gL*(V_thresh-El)]*int((500*(10**-3)/delta_t)))
 # Ic1 = np.array([gL*(V_thresh-El+0.0001)]*int((500*(10**-3)/delta_t)))
@@ -67,12 +84,12 @@ for i in range(num_neurons):
     I[i,:] = (1+i*0.1)*gL*(V_thresh-El)*I[i,:]
 print(I.shape)
 
-neurons = LIF(C, gL, V_thresh, El, num_neurons=num_neurons)
+neurons = LIF(C, gL, V_thresh, El, Rp, num_neurons=num_neurons)
 V0 = np.ones(shape=(10,1))
 V0 = El*V0
 V = neurons.compute(V0, I, delta_t)
 
-plt.figure(figsize=(15,13))
+plt.figure(figsize=(175,135))
 plt.subplot(2,2,1)
 plt.plot(V[1,:], 'r', label='I={:.4f} X e-9'.format(I[1,0]*10**9))
 plt.ylabel('potential')
